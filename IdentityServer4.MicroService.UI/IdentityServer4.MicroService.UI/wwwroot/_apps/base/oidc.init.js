402a90568b985f88430a05893e14b1b4b282febf
王振 <361790599@qq.com>
2018-07-04 16:13:02 +0800

    updated

--- IdentityServer4.MicroService.UI/IdentityServer4.MicroService.UI/wwwroot/_apps/base/oidc.init.js
+++ IdentityServer4.MicroService.UI/IdentityServer4.MicroService.UI/wwwroot/_apps/base/oidc.init.js
@@ -1,17 +1,28 @@
-﻿Oidc.Log.logger = console;
-Oidc.Log.level = Oidc.Log.WARN;
-var siteurl = location.protocol + "//" + location.host;
-var settings = {
-    authority: 'https://localhost:44309',
-    client_id: 'adminportal',
-    redirect_uri: siteurl + '/callback.html',
-    post_logout_redirect_uri: siteurl + '/logout.html',
-    response_type: 'id_token token',
-    scope: 'openid profile ids4.ms.all',
-    silent_redirect_uri: siteurl + '/silent_callback.html',
-    automaticSilentRenew: true,
-    //silentRequestTimeout:10000,
-    filterProtocolClaims: true,
-    loadUserInfo: true
-};
-window.oidc = new Oidc.UserManager(settings);
+﻿var _authority = localStorage.getItem("oidc_authority");
+var _client_id = localStorage.getItem("oidc_client_id");
+
+if ((_authority == null || _client_id == null) && location.pathname !='/start.html')
+{
+    location.href = '/start.html';
+}
+else {
+
+    Oidc.Log.logger = console;
+    Oidc.Log.level = Oidc.Log.WARN;
+    var siteurl = location.protocol + "//" + location.host;
+
+    var settings = {
+        authority: _authority,
+        client_id: _client_id,
+        redirect_uri: siteurl + '/callback.html',
+        post_logout_redirect_uri: siteurl + '/logout.html',
+        response_type: 'id_token token',
+        scope: 'openid profile ids4.ms.all',
+        silent_redirect_uri: siteurl + '/silent_callback.html',
+        automaticSilentRenew: true,
+        //silentRequestTimeout:10000,
+        filterProtocolClaims: true,
+        loadUserInfo: true
+    };
+    window.oidc = new Oidc.UserManager(settings);
+}
